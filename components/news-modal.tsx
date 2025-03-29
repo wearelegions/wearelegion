e16938d0ce8810620480d499967bd8561e9bf3d8
wearelegion <workingscholars4@gmail.com>
2025-03-29 14:21:11 +0800

    news modal

--- components/news-modal.tsx
+++ components/news-modal.tsx
@@ -1,10 +1,10 @@
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog"
+import { ScrollArea } from "@/components/ui/scroll-area"
 
 type NewsModalProps = {
   isOpen: boolean
@@ -19,14 +19,18 @@ type NewsModalProps = {
 export function NewsModal({ isOpen, onClose, news }: NewsModalProps) {
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="bg-hacker-terminal border border-hacker-primary/30">
-        <DialogHeader>
-          <DialogTitle className="text-hacker-primary font-hack">{news.title}</DialogTitle>
+      <DialogContent className="bg-hacker-terminal border border-hacker-primary/30 w-[90vw] max-w-2xl h-[90vh] max-h-[800px] p-0">
+        <DialogHeader className="p-4 sm:p-6 border-b border-hacker-primary/30">
+          <DialogTitle className="text-hacker-primary font-hack text-base sm:text-lg">{news.title}</DialogTitle>
           <div className="text-xs text-hacker-primary/50 font-hack">{news.date}</div>
         </DialogHeader>
-        <DialogDescription className="text-hacker-primary/70 font-hack whitespace-pre-wrap">
-          {news.content}
-        </DialogDescription>
+        <ScrollArea className="flex-1 h-[calc(90vh-8rem)] max-h-[calc(800px-8rem)]">
+          <div className="p-4 sm:p-6">
+            <div className="text-hacker-primary/70 font-hack text-sm sm:text-base whitespace-pre-wrap">
+              {news.content}
+            </div>
+          </div>
+        </ScrollArea>
       </DialogContent>
     </Dialog>
   )
